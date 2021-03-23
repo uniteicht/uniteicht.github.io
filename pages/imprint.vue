2dbdd6cbee3b7c30f1d8b56d0e228c2b79568da5
Uniteicht <uniteicht>
2021-03-24 00:49:43 +0100

    sinnvolle Vertretungsarbeit

---
+++ pages/imprint.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wr-imprint">
+    <h2>Herausgeber und Medieninhaber dieser Satireseite:</h2>
+    <p>
+      Jakob Wögerbauer<br />
+      Mengerstraße 23/503<br />
+      4040 Linz
+    </p>
+
+    <p>
+      E-Mail:
+      <a href="mailto:hario.mofer@uniteicht.at">hario.mofer@uniteicht.at</a>
+    </p>
+
+    <h2>Credits</h2>
+    <p>
+      Icon (<img class="icon" src="/img/external-link-symbol.svg" />) made by
+      <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy"
+        >Dave Gandy</a
+      >
+      from
+      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </p>
+
+    <h2>Webdesign</h2>
+    <p>
+        Das Design dieser Seite wurde bewusst hässlich gehalten, um den Satirecharacter hervorzuheben.
+    </p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {};
+</script>
+
+<style>
+.wr-imprint {
+  font-size: large;
+}
+</style>
