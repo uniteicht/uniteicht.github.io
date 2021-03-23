c6aad9d3ea5f45bda205305795be98c183f493b0
Uniteicht <uniteicht>
2021-03-23 20:17:54 +0100

    sinnvolle Vertreterarbeit

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <template>
-    <h1>Memes for everyone!</h1>
-
+  <div class="wrapper">
+    <h1>FÜR DICH ERREICHT</h1>
+  </div>
 </template>
 
 <style>
-
 </style>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
+.wrapper {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  padding-left: 10px;
+}
+
+h1 {
+  color: black;
+}
+</style>
