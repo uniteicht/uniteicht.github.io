6b9db92491e034b93628d8f9cde650dfd064ccbe
Uniteicht <uniteicht>
2021-03-23 20:38:55 +0100

    sinnvolle Vertreterarbeit

---
+++ components/Meme.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="meme">
+    <img :src="'/meme/' + image" />
+    <h3>{{ text }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+    },
+    text: {
+      type: String,
+      default() {
+        return "sinnvolle Vertreterarbeit!";
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.meme {
+  width: 80%;
+  border: 1px solid;
+}
+
+img {
+  max-height: 500px;
+}
+</style>
---
+++ components/MemeList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mlWrapper">
+    <meme :image="'RIP_Steuerrecht.png'" :text="'Mein Text'" />
+  </div>
+</template>
+
+<script>
+import Meme from "./Meme.vue";
+export default {
+  components: { Meme },
+};
+</script>
+
+<style>
+.mlWrapper {
+  width: 100%;
+}
+</style>
