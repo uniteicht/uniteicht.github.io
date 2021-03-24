507cdb3125f9d1e1524c0536dd8e7a82cd30ed25
uniteicht <uniteicht@users.noreply.github.com>
2021-03-24 14:42:13 +0000

    deploy: 983bf807883f209f9ae55973513d7236f9df4f7f

---
+++ comments/comments.js
@@ -0,0 +1,52 @@
+const hmCommentEntries = [
+    {
+        name: "Hario Mofer",
+        img: "/img/profilepic.png",
+        text: "Das hab ICH aber gut gemacht.",
+    },
+    {
+        name: "Hario Mofer",
+        img: "/img/profilepic.png",
+        text: "Gute arbeit ICH.",
+    },
+    {
+        name: "Hario Mofer",
+        img: "/img/profilepic.png",
+        text: "ICH bin immer wieder von mir überzeugt.",
+    },
+];
+
+const commentEntries = [
+    {
+        name: "Not a bot 1",
+        img: "/img/profilepic.png",
+        text:
+            "Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla. Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla.",
+    },
+    {
+        name: "Not a bot 2",
+        img: "/img/profilepic.png",
+        text:
+            "Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla. Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla.",
+    },
+    {
+        name: "Not a bot 3",
+        img: "/img/profilepic.png",
+        text:
+            "Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla. Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla.",
+    },
+    {
+        name: "Not a bot 4",
+        img: "/img/profilepic.png",
+        text:
+            "Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla. Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla.",
+    },
+    {
+        name: "Not a bot 5",
+        img: "/img/profilepic.png",
+        text:
+            "Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla. Bla bla bla bla bla bla bla bla bla ICH bla bla bla bla.",
+    },
+];
+
+export { commentEntries, hmCommentEntries };
