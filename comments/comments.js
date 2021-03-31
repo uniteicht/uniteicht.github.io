8992e073f451869364b659550fe250131d6282f8
uniteicht <uniteicht@users.noreply.github.com>
2021-03-31 16:01:47 +0000

    deploy: f289eb179cc8b4c23ebb807653f435d6d530703b

--- comments/comments.js
+++ comments/comments.js
@@ -1,18 +1,18 @@
 const hmCommentEntries = [{
-        name: "Hario Mofer",
-        img: "/img/Hario_Mofer.png",
-        text: "Das hab ICH aber gut gemacht.",
-    },
-    {
-        name: "Hario Mofer",
-        img: "/img/Hario_Mofer.png",
-        text: "Gute arbeit ICH.",
-    },
-    {
-        name: "Hario Mofer",
-        img: "/img/Hario_Mofer.png",
-        text: "ICH bin immer wieder von mir überzeugt.",
-    },
+  name: "Hario Mofer",
+  img: "/img/Hario_Mofer.png",
+  text: "Das hab ICH aber gut gemacht.",
+},
+  {
+    name: "Hario Mofer",
+    img: "/img/Hario_Mofer.png",
+    text: "Gute arbeit ICH.",
+  },
+  {
+    name: "Hario Mofer",
+    img: "/img/Hario_Mofer.png",
+    text: "ICH bin immer wieder von mir überzeugt.",
+  },
 ];
 
 /*
@@ -20,44 +20,44 @@ Random name generator: https://de.fakenamegenerator.com/gen-random-gr-as.php
 Profile pictures by https://generated.photos/
 */
 const commentEntries = [{
-        name: "Juliane Bumgarner",
-        img: "/img/profilepic1.png",
-        text: "Großartig gedichtet😍",
-    }, {
-        name: "Ulrike Herz",
-        img: "/img/profilepic2.png",
-        text: "So gehörts! 🙌",
-    },
-    {
-        name: "Mario König",
-        img: "/img/profilepic3.jpg",
-        text: "Richtig so! 💪",
-    },
-    {
-        name: "Sophie Pfaff",
-        img: "/img/profilepic4.jpg",
-        text: "So was ist einfach nur peinlich von gewissen anderen Fraktionen! ✊",
-    },
-    {
-        name: "Leah Holzman",
-        img: "/img/profilepic5.png",
-        text: "Vorbildlichst, so fühlt man sich als Studierender bestmöglich vertreten 👏😎!",
-    },
-    {
-        name: "Markus Schweizer",
-        img: "/img/profilepic6.png",
-        text: "Super geschrieben 😍👏",
-    },
-    {
-        name: "Ralf Maier",
-        img: "/img/profilepic7.png",
-        text: "Mega cool 🤩👍",
-    },
-    {
-        name: "Katrin Wulf",
-        img: "/img/profilepic3.jpg",
-        text: "So solls sein! 🧡",
-    },
+  name: "Buliane Jumgarner",
+  img: "/img/profilepic1.png",
+  text: "Großartig gedichtet😍",
+}, {
+  name: "Hulrike Erz",
+  img: "/img/profilepic2.png",
+  text: "So gehörts! 🙌",
+},
+  {
+    name: "Kario Mönig",
+    img: "/img/profilepic3.jpg",
+    text: "Richtig so! 💪",
+  },
+  {
+    name: "Pophie Sauff",
+    img: "/img/profilepic4.jpg",
+    text: "So was ist einfach nur peinlich von gewissen anderen Fraktionen! ✊",
+  },
+  {
+    name: "Heah Lolzman",
+    img: "/img/profilepic5.png",
+    text: "Vorbildlichst, so fühlt man sich als Studierender bestmöglich vertreten 👏😎!",
+  },
+  {
+    name: "Schwarkus Meizer",
+    img: "/img/profilepic6.png",
+    text: "Super geschrieben 😍👏",
+  },
+  {
+    name: "Malf Raier",
+    img: "/img/profilepic7.png",
+    text: "Mega cool 🤩👍",
+  },
+  {
+    name: "Watrin Kulf",
+    img: "/img/profilepic3.jpg",
+    text: "So solls sein! 🧡",
+  },
 ];
 
 export { commentEntries, hmCommentEntries };
